6fcaa71dc1f62041253939d36b3bc91df6459960
Thomas Sauerwald <sauerwald@users.noreply.github.com>
2022-02-03 16:05:01 +0100

    Added a number generator with values between -100 and 99 which gives the result as a csv file.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string resultPath = @"../../../input.csv";
+int quantity = 10000000;
+int minValue = -100;
+int maxValue = 100;
+
+// Generate numbers
+Random numberGenerator = new Random();
+int[] result = new int[quantity];
+for (int i = 0; i < quantity; i++)
+{
+    var number = numberGenerator.Next(minValue, maxValue);
+    result[i] = number;
+}
+
+// Save numbers to file
+using (FileStream stream = File.Open(resultPath, FileMode.Create))
+{
+    using (StreamWriter writer = new StreamWriter(stream))
+    {
+        for (int i = 0; i < result.Length - 1; i++)
+        {
+            writer.Write(result[i] + ",");
+        }
+        writer.Write(result[result.Length - 1]);
+    }
+}
